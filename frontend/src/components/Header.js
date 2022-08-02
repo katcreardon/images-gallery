debd175e354f5fcc1be629197e9f67381a0c3e29
Kathryn Reardon <kathryn.reardon@lexmark.com>
2022-08-02 09:16:37 -0400

    fixed code with linter

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStyle = {
-    backgroundColor: 'lightpink'
+  backgroundColor: 'lightpink',
 };
 
 const Header = ({ title }) => {
-    return (
-        <Navbar style={navbarStyle} variant="light">
-            <Container>
-                <Navbar.Brand href="/">{title}</Navbar.Brand>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
